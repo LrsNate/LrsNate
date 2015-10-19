995732ad376429ee8282180325c0d8565fc6c26e
Antoine LAFOUASSE <antoine.lafouasse@icloud.com>
2015-10-19 17:48:12 +0200

    Implementing unit on map

--- public/aw/js/controllers/controllers.js
+++ public/aw/js/controllers/controllers.js
@@ -35,13 +35,32 @@ controllers.controller('GameController', [
             ]
         ];
 
+        var units = [
+            {
+                type: "inftr",
+                x: 1,
+                y: 1
+            }
+        ];
+
+        $scope.selectedUnit = {};
+
+        angular.forEach(units, function (u) {
+            $scope.grid[u.y][u.x].unit = {type: u.type};
+        });
+
         $scope.cellClick = function (cell, x, y) {
             angular.element('.cell').removeClass('selected');
             angular.element('.x' + x + '.y' + y).addClass('selected');
         };
 
         $scope.cellClasses = function (cell, x, y) {
-            return [cell.type, 'x' + x, 'y' + y];
+            var base = [cell.type, 'x' + x, 'y' + y];
+            if (cell.unit) {
+                base.push('unit');
+                base.push('unit-' + cell.unit.type);
+            }
+            return base;
         };
     }
 ]);
